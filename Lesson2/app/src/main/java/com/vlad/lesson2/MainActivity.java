08e299a7a0ede378715c4954bb8bc8375469f4e9
Vlad Maskaikin <vlad.maskajkin@gmail.com>
2018-12-25 21:51:32 +0300

    Исправление

--- Lesson2/app/src/main/java/com/vlad/lesson2/MainActivity.java
+++ Lesson2/app/src/main/java/com/vlad/lesson2/MainActivity.java
@@ -1,13 +1,13 @@
 package com.vlad.lesson2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int[] fibonachi;
-    private int[] fourthPoint = {10,3,7,2,5,8};
+    private int[] fibonacci;
+    private int[] fourthPoint = {10, 3, 7, 2, 5, 8};
 
     final double a = 4.5;
     final double b = 3.5;
@@ -24,12 +24,12 @@ public class MainActivity extends AppCompatActivity {
 
         // 2-ой пункт
         SecondPoint people = new SecondPoint();
-      System.out.println("Full name: "+ people.firstName+ " "+ people.lastName+" ");
+        System.out.println("Full name: " + people.firstName + " " + people.lastName + " ");
         // конец 2-ого пункта
 
         //3-ий пункт
-        getFibonachi(15);
-        showFibonachi(fibonachi);
+        initFibonacci(15);
+        showFibonacci();
         //конец 3-ого пункта
 
         //4-ый пукнт
@@ -39,46 +39,51 @@ public class MainActivity extends AppCompatActivity {
         //5-ый пукнт
         String fivePoint = "abc123";
         String newString = fivePoint(fivePoint);
-        Log.d("fivePointt", fivePoint);
         Log.d("fivePoint", newString);
         //конец 5-ого пункта
     }
 
     double getAverage(double a, double b) {
-        return (a+b)/2;
+        return (a + b) / 2;
     }
 
-    void getFibonachi (int n) {
-        fibonachi = new int[n];
-        fibonachi[0] = 0;
-        fibonachi[1] = 1;
+    void initFibonacci(int n) {
+        fibonacci = new int[n];
+        fibonacci[0] = 0;
+        fibonacci[1] = 1;
         for (int i = 2; i < n; ++i) {
-            fibonachi[i] = fibonachi[i - 1] + fibonachi[i - 2];
+            fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
         }
     }
 
-    void showFibonachi (int[] fibonachi) {
-        for (int i = 0; i < fibonachi.length; ++i) {
-            System.out.println(fibonachi[i]);
+    void showFibonacci() {
+        for (int valueFibonacci : fibonacci) {
+            System.out.println(valueFibonacci);
         }
     }
 
-    void sortArray (int[] arrayInt) {
-        for (int i = 0; i < arrayInt.length-1; i++) {
-            if(arrayInt[i] > arrayInt[i+1]){
-                int saveValue = arrayInt[i];
-                arrayInt[i] = arrayInt[i+1];
-                arrayInt[i+1] = saveValue;
+    void sortArray(int[] arrayInt) {
+        for (int k = arrayInt.length - 1; k >= 1; k--) {
+            for (int i = 0; i < arrayInt.length - 1; i++) {
+                if (arrayInt[i] > arrayInt[i + 1]) {
+                    int saveValue = arrayInt[i];
+                    arrayInt[i] = arrayInt[i + 1];
+                    arrayInt[i + 1] = saveValue;
+                }
             }
         }
     }
 
     String fivePoint(String string) {
-        int numbers= Integer.parseInt(string.replaceAll("[^0-9]", ""));
-        int newNumbers = numbers+1;
-        return string.replaceAll(String.valueOf(numbers), String.valueOf(newNumbers));
+        try {
+            int numbers = Integer.parseInt(string.replaceAll("[^0-9]", ""));
+            int newNumbers = numbers + 1;
+            return string.replaceAll(String.valueOf(numbers), String.valueOf(newNumbers));
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            return "Не получилось";
+        }
     }
-
 
     public class SecondPoint {
         String firstName = "Влад";
