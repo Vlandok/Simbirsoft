df03f5dbadaa4351a3bc5105a0645376587aad98
Vlad Maskaikin <vlad.maskajkin@gmail.com>
2019-01-21 20:46:15 +0300

    no message

--- Lesson4/app/src/main/java/com/vlad/lesson4/presentation/ui/main/MainMvpView.java
+++
@@ -1,9 +0,0 @@
-package com.vlad.lesson4.presentation.ui.main;
-
-import com.vlad.lesson4.presentation.ui.base.MvpView;
-
-public interface MainMvpView extends MvpView {
-    void setTextInTextViewToolbar(String titleToolbar);
-
-    void clickButtonBottomNav();
-}
--- Lesson4/app/src/main/java/com/vlad/lesson4/presentation/ui/splashscreen/SplashScreenMvpView.java
+++
@@ -1,7 +0,0 @@
-package com.vlad.lesson4.presentation.ui.splashscreen;
-
-import com.vlad.lesson4.presentation.ui.base.MvpView;
-
-public interface SplashScreenMvpView extends MvpView {
-    void startMainActivity();
-}
