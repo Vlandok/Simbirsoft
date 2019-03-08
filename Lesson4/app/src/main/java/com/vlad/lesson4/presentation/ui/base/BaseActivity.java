b4dd2d89a59f5f7fc7c6a73dbb8b7bdfd090db9c
Vlad Maskaikin <vlad.maskajkin@gmail.com>
2019-03-08 12:34:26 +0300

    no message

--- Lesson4/app/src/main/java/com/vlad/lesson4/presentation/ui/base/BaseActivity.java
+++ Lesson4/app/src/main/java/com/vlad/lesson4/presentation/ui/base/BaseActivity.java
@@ -3,26 +3,13 @@ package com.vlad.lesson4.presentation.ui.base;
 import android.os.Bundle;
 
 import com.jakewharton.threetenabp.AndroidThreeTen;
-import com.vlad.lesson4.MyApplication;
 import com.vlad.lesson4.androidx.MvpAppCompatActivity;
-import com.vlad.lesson4.di.component.ActivityComponent;
-import com.vlad.lesson4.di.module.ActivityModule;
 
 public abstract class BaseActivity extends MvpAppCompatActivity {
-    private ActivityComponent activityComponent;
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AndroidThreeTen.init(this);
-//        activityComponent = DaggerActivityComponent.builder()
-//                .activityModule(new ActivityModule(this))
-////                .applicationComponent(((MyApplication) getApplication()).getApplicationComponent())
-//                .applicationComponent(MyApplication.applicationComponent)
-//                .build();
-    }
-
-    public ActivityComponent getActivityComponent() {
-        return activityComponent;
     }
 }
